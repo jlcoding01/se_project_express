ac384a01289ea22b088a00cecec2298017755c66
Jerry Liu <ljliu89@gmail.com>
2024-08-21 17:35:09 -0700

    fix some bugs

--- controllers/users.js
+++ controllers/users.js
@@ -51,7 +51,7 @@ const createUser = (req, res) => {
       return bcrypt.hash(password, 10);
     })
     .then((hash) => {
-      User.create({ name, avatar, email, password: hash });
+      return User.create({ name, avatar, email, password: hash });
     })
     .then((user) => {
       res.status(201).send({
